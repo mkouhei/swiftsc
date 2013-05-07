3d6e87f410967e1e7a436184303b9cd16b97ea40
Kouhei Maeda <mkouhei@palmtb.net>
2013-05-07 11:42:33 +0900

    increate version and update document
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ long_description = \
 requires = ['setuptools', 'requests']
 
 setup(name='swiftsc',
-      version='0.1.1',
+      version='0.1.2',
       description='Simple client library of OpenStack Swift',
       long_description=long_description,
       author='Kouhei Maeda',
