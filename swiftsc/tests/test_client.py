cbf34774739523b29e8ff465a4780f7a9c277811
Kouhei Maeda <mkouhei@palmtb.net>
2015-02-27 12:35:32 +0900

    Fixed importing json module.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- swiftsc/tests/test_client.py
+++ swiftsc/tests/test_client.py
@@ -16,6 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import os.path
+import json
 import unittest
 from httpretty import HTTPretty, httprettified
 from swiftsc import client
