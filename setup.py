2af0cd1cdb955632e3cada05888ae408e94ca74f
Kouhei Maeda <mkouhei@palmtb.net>
2015-03-11 12:40:01 +0900

    Bump version.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ long_description = (
 requires = ['setuptools', 'requests', 'python-magic']
 
 setup(name='swiftsc',
-      version='0.6.1',
+      version='0.6.2',
       description='Simple client library of OpenStack Swift',
       long_description=long_description,
       author='Kouhei Maeda',
